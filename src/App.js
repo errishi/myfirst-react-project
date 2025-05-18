0738f775b3f157126bbd0712b7cb061c159dd4eb
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-05-18 22:49:14 +0530

    sending parent data to child by props

--- src/App.js
+++ src/App.js
@@ -2,23 +2,78 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
+import { Card, Button, Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="main">
-      <Header/>
-      <div className='row'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <Header email="ws@gmail.com" phone="8293728738" />
+      <div className='container'>
+        <Container fluid>
+          <Container>
+            <Row>
+              <Col className='col-12 text-center py-4'>
+                <h1>Our Courses</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="3" md="6">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Course1</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col lg="3" md="6">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Course1</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col lg="3" md="6">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Course1</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col lg="3" md="6">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Course1</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
       </div>
-      <Footer/>
+      <Footer />
 
 
     </div>
@@ -26,10 +81,3 @@ function App() {
 }
 
 export default App;
-
-
-function Card(){
-  return(
-    <div className='cardItems'>Card div</div>
-  )
-}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,9 @@
-function Header(){
+function Header(props){
+    console.log(props.email)
+
     return(
         <div>
-            <h1>Welcome Header section</h1>
+            <h1>Welcome Header section {props.email} | {props.phone} </h1>
             <div>Hello world</div>
             <div>React project</div>
         </div>
